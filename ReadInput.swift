66ebd9ca5b6714760646d3b9b036e6626bb93fd7
krishanVir <krishanvir.singh@vuliv.com>
2019-08-13 10:29:07 +0530

    readInput in Swift

---
+++ ReadInput.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  HackerEarthPracticeQuestions
+//
+//  Created by Administrator on 13/08/19.
+//  Copyright © 2019 Administrator. All rights reserved.
+//
+
+import Foundation
+
+print("Enter  Input value ")
+let inputInteger = readLine()
+
+if let inputValue = Int(inputInteger!) {
+    print(inputValue)
+}
+
+print("Enter  String ")
+let stringInput =  readLine()
+print(stringInput!)
