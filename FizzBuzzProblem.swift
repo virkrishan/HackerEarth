0b526e2691eb63efa3c30a47f5ce008196d5f82e
krishanVir <krishanvir.singh@vuliv.com>
2019-08-23 14:15:26 +0530

    FizzBuzz Problem added

---
+++ FizzBuzzProblem.swift
@@ -0,0 +1,31 @@
+//
+//  FizzBuzzProblem.swift
+//  
+//
+//  Created by Administrator on 23/08/19.
+//
+
+import Foundation
+
+//: Playground - noun: a place where people can play
+
+let thousandNumbers = [Int](1...1000)
+
+func printFizzBuzz(numbers: [Int]) {
+    for i in numbers {
+        if i % 15 == 0 {
+            print("\(i) fizzbuzz")
+        }
+        else if i % 3 == 0 {
+            print("\(i) fizz")
+        }
+        else if i % 5 == 0 {
+            print("\(i) buzz")
+        }
+        else {
+            print("\(i)")
+        }
+    }
+}
+
+printFizzBuzz(numbers: thousandNumbers)
